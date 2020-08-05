779ba67504d4c6a1dd444b38459f6d7656861c4e
acln <andreas@acln.dk>
2020-08-05 07:23:04 +0200

    Bed size redused to 230x230

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1114,8 +1114,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 230
+#define Y_BED_SIZE 230
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1363,8 +1363,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS -2.5
-#define MANUAL_Y_HOME_POS -12.5
+#define MANUAL_X_HOME_POS -3.5
+#define MANUAL_Y_HOME_POS -12.0
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
